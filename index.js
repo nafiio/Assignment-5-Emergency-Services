4f1c913c010575d75e78e39b37d55f95d25dd57b
nafiio <aznafix@gmail.com>
2026-03-17 05:37:40 +0600

    added the heart icon functionality

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("index js connected")
+
+// heart icon clicked
+
+const heartClicked = document.getElementsByClassName("heart");
+console.log(heartClicked);
+for (const heart of heartClicked) {
+  heart.addEventListener("click", function () {
+    const heartBeat = document.getElementById("heart-beat");
+    const heartBeatValue = heartBeat.innerText;
+    const heartCount = Number(heartBeatValue) + 1;
+    heartBeat.innerText = heartCount;
+  });
+}
+
+
+// Call button
